d81ff1246a7fa151da5da6496fcd756691a1508d
TwilightArc3196 <muhammad.ahmed.k346@gmail.com>
2025-07-14 23:42:25 +0200

    make start script use virtual environment

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uvicorn app.main:app --host 0.0.0.0 --port 8002 &
-PYTHONPATH=$(pwd) streamlit run ui/dashboard.py --server.port 8502 --server.headless true
+../venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8010 &
+PYTHONPATH=$(pwd) ../venv/bin/streamlit run ui/dashboard.py --server.port 8510 --server.headless true
